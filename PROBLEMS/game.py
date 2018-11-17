d314690bc359f23dbca61f5fe6aec7581c3c47af
ale-cci <alecrd98@gmail.com>
2018-11-17 17:13:28 +0100

    Improving readability

---
+++ PROBLEMS/game.py
@@ -0,0 +1,28 @@
+
+class GameInterface:
+    def reset(self):
+        """ When called the game should be resetted at the initial condition """
+        raise NotImplementedError()
+
+    def won(self) -> bool:
+        """ Method called to check if the """
+        raise NotImplementedError()
+
+    def lost(self) -> bool:
+        """ Returns True if the player has lost in the given game status """
+        raise NotImplementedError()
+
+    def do_action(self, encoded_action) -> None:
+        """ Execute the current player action """
+        raise NotImplementedError()
+
+    def get_actions(self) -> list:
+        """ Get the possible actions that the player can do """
+        raise NotImplementedError()
+
+    def draw(self) -> None:
+        pass
+    
+    def encoded_status(self):
+        """ Returns an unique codification of the current game status """
+        raise NotImplementedError()
--- PROBLEMS/labirinth.py
+++ PROBLEMS/labirinth.py
@@ -1,6 +1,7 @@
 # Game Rules:
-possible_actions = [(0, -1), (0, 1), (-1, 0), (1, 0)]	# Top Down Left Right
-initial_status = (0, 0)			# Start position
+
+possible_actions = [(0, -1), (0, 1), (-1, 0), (1, 0)]   # Top Down Left Right
+initial_status = (0, 0)                 # Start position
 
 _hidden_obstacles = [
 ]
@@ -9,16 +10,57 @@ _game_w = 5
 _game_h = 5
 
 def won(status):
-	return status == (_game_w, _game_h)
+        return status == (_game_w, _game_h)
 
 def lost(status):
-	return status in _hidden_obstacles
+        return status in _hidden_obstacles
 
 def next_status(status, action):
-	dx, dy = possible_actions[action]
-	x, y = status
+        dx, dy = possible_actions[action]
+        x, y = status
 
-	# Bounding new Coords
-	x0 = min(max(0, x+dx), _game_w)
-	y0 = min(max(0, y+dy), _game_h)
-	return x0, y0
+        # Bounding new Coords
+        x0 = min(max(0, x+dx), _game_w)
+        y0 = min(max(0, y+dy), _game_h)
+        return x0, y0
+
+from PROBLEMS.game import GameInterface
+from collections import namedtuple
+
+def bound(value, minimum, maximum):
+    return min(max(minimum, value), maximum)
+
+class Game(GameInterface):
+    def __init__(self):
+        self._hidden_obstacles = []
+
+        Coordinates = namedtuple('Coordinates', ('x', 'y'))
+        self.actions = [
+            Coordinates(0, -1),
+            Coordinates(0, +1),
+            Coordinates(-1, 0),
+            Coordinates(+1, 0)
+        ]
+
+        self.board = Coordinates(5, 5)
+
+        self._goal = [4, 4]
+    def reset(self):
+        self._position = [0, 0]
+
+    def do_action(self, action):
+        self._position[0] = bound(self._position[0] + action.x, minimum=0, maximum=self.board.x)
+        self._position[1] = bound(self._position[1] + action.y, minimum=0, maximum=self.board.y)
+
+
+    def lost(self):
+        return self._position in self._hidden_obstacles
+
+    def won(self):
+        return self._position == self._goal
+
+    def get_actions(self) -> list:
+        return self.actions
+
+    def encoded_status(self):
+        return str(self._position)
